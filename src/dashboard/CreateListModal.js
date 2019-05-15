1c8a87db1c2cd9f3162c7bd713b152fcb3829a1d
Heather Riddle <heather0809@gmail.com>
2019-05-14 21:27:16 -0500

    Build UI to create a new list [#165656779]

---
+++ src/dashboard/CreateListModal.js
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+
+class CreateListModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listName: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({listName: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.submit(this.state.listName, this.props.ownerId)
+  }
+
+  submit(listName, userId) {
+    fetch(`${process.env.REACT_APP_API_HOST}/users/${userId}/lists`, {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: listName
+    }).then(res => res.json())
+      .then(list => this.props.refreshLists())
+      .catch(err => alert(`Creating list was unsuccessful:\n\n${err}`));
+    this.props.closeModal();
+  }
+
+  render() {
+    return (
+      <div className="create-list-modal">
+        <form onSubmit={this.handleSubmit}>
+          <div className="field-list-name">
+            <label htmlFor="list-name" className="field-label">List Name</label>
+            <input id="list-name" name="listName" className="field" type="text" value={this.state.listName}
+                   onChange={this.handleChange.bind(this)}/>
+          </div>
+          <div className="button-row">
+            <input className="cancel-button" type="submit" value="Cancel" onClick={this.props.closeModal}/>
+            <input className="submit-button" type="submit" value="Create"/>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default CreateListModal
--- src/dashboard/DashboardView.js
+++ src/dashboard/DashboardView.js
@@ -1,49 +1,80 @@
 import React, {Component} from 'react';
 import './DashboardView.css';
 import logo from "../assets/logo-light-with-text.svg";
+import CreateListModal from "./CreateListModal";
 
 class DashboardView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openCreatePersonalListModal: false
+    };
+    this.closeCreateListModal = this.closeCreateListModal.bind(this);
+    this.refreshPersonalLists = this.refreshPersonalLists.bind(this);
+  }
 
-    render() {
-        return (
-            <div className="dashboardView">
-                <div className='header-row'>
+  createList() {
+    this.setState({openCreatePersonalListModal: true});
+  }
 
-                    <div className="header-title">
-                        <div className="header-text">DASHBOARD</div>
-                        <div className="name-container">
-                            <div className="header-user-name">{this.props.user.firstName}<br/> {this.props.user.lastName}</div>
-                            <div className="circle-thing">
-                                <div className="header-initials">{this.props.user.firstName.charAt(0)}{this.props.user.lastName.charAt(0)}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="logo-container">
-                        <img src={logo} alt="Team Tasks logo" className="logo-img"/>
-                    </div>
-                </div>
-
-                <div className="sidebox">
-                    <div className="divider"></div>
-                    <div className="list">
-                        <div className="list-title">Personal Lists</div>
-                        {/*<div className="list-element">To Do List</div>*/}
-                        {/*<div className="list-element">Training Due</div>*/}
-                        <div className="new-list">NEW LIST</div>
-                    </div>
-                    <div className="divider"></div>
-                    <div className="list">
-                        <div className="list-title">Teams</div>
-                        {/*<div className="list-element">Stark Industries</div>*/}
-                        {/*<div className="list-element">S.H.I.E.L.D</div>*/}
-                        {/*<div className="list-element">The Avengers</div>*/}
-                        {/*<div className="list-element">Guardians of the Galaxy</div>*/}
-                        <div className="new-list">NEW LIST</div>
-                    </div>
-                </div>
-            </div>
-        )
+  renderModals() {
+    if (this.state.openCreatePersonalListModal) {
+      return <CreateListModal closeModal={this.closeCreateListModal} ownerId={this.props.user.userId} refreshLists={this.refreshPersonalLists}/>
     }
+  }
+
+  closeCreateListModal() {
+    this.setState({openCreatePersonalListModal: false});
+  }
+
+  refreshPersonalLists() {
+    console.log("a list was created so the navbar needs to be re-rendered");
+  }
+
+  render() {
+    return (
+      <div className="dashboardView">
+        <div className='header-row'>
+
+          <div className="header-title">
+            <div className="header-text">DASHBOARD</div>
+            <div className="name-container">
+              <div className="header-user-name">{this.props.user.firstName}<br/> {this.props.user.lastName}</div>
+              <div className="circle-thing">
+                <div
+                  className="header-initials">{this.props.user.firstName.charAt(0)}{this.props.user.lastName.charAt(0)}</div>
+              </div>
+            </div>
+          </div>
+          <div className="logo-container">
+            <img src={logo} alt="Team Tasks logo" className="logo-img"/>
+          </div>
+        </div>
+
+        <div className="sidebox">
+          <div className="divider"/>
+          <div className="list">
+            <div className="list-title">Personal Lists</div>
+            {/*<div className="list-element">To Do List</div>*/}
+            {/*<div className="list-element">Training Due</div>*/}
+            <div className="new-list" onClick={this.createList.bind(this)}>NEW LIST</div>
+          </div>
+          <div className="divider"/>
+          <div className="list">
+            <div className="list-title">Teams</div>
+            {/*<div className="list-element">Stark Industries</div>*/}
+            {/*<div className="list-element">S.H.I.E.L.D</div>*/}
+            {/*<div className="list-element">The Avengers</div>*/}
+            {/*<div className="list-element">Guardians of the Galaxy</div>*/}
+            <div className="new-list">NEW LIST</div>
+          </div>
+        </div>
+        <div className="main-content">
+          {this.renderModals()}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default DashboardView
