aaf4a532529d5d7d59dac0d1c2f2597eb15b54ae
Paul Warren <paul_warren@bcbsil.com>
2019-05-21 18:56:50 -0500

    Final fixes for team display page [finishes #165588671]

--- src/dashboard/AllTeamsView.js
+++ src/dashboard/AllTeamsView.js
@@ -2,42 +2,47 @@ import React, {Component} from 'react';
 import './AllTeamsView.css';
 
 class AllTeamsView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            teams: []
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      teams: []
+    };
+  }
 
-    componentDidMount() {
-        this.fetchAllTeams()
-    }
+  componentDidMount() {
+    this.fetchAllTeams()
+  }
 
-    fetchAllTeams() {
-        fetch(`${process.env.REACT_APP_API_HOST}/teams`, {
-            method: "GET",
-            headers: {"Content-Type": "application/json"}
-        }).then(res => res.json())
-            .then(lists => this.setState({teams: lists}))
-            .catch(err => alert(`Fetching teams was unsuccessful:\n\n${err}`))
-    }
+  fetchAllTeams() {
+    fetch(`${process.env.REACT_APP_API_HOST}/teams`, {
+      method: "GET",
+      headers: {"Content-Type": "application/json"}
+    }).then(res => res.json())
+      .then(lists => this.setState({teams: lists}))
+      .catch(err => alert(`Fetching teams was unsuccessful:\n\n${err}`))
+  }
 
-    render() {
-        let teams = [];
-        if (this.state.teams.length > 0) {
-            teams = this.state.teams.map(team =>
-                <div className="trow">
-                    <div className="team" key={team.id}>{team.name}</div><div className="join">JOIN</div>
-                    <div className="team-divider"/>
-                </div>
-            );
-        }
-        return (
-            <div className="teams">
-                <div className="team-divider"/>
-                {teams}
-            </div>)
+  render() {
+    let teams = [];
+    if (this.state.teams.length > 0) {
+      teams = this.state.teams.map(team =>
+        <li className="team" key={team.id}>
+          <div className="team-name" >{team.name}</div>
+          <div className="join">JOIN</div>
+          <div className="team-divider"/>
+        </li>
+      );
     }
+    return (
+      <div className="teams">
+        <div className="team-container">
+          <div className="team-divider"/>
+          <ul>
+            {teams}
+          </ul>
+        </div>
+      </div>)
+  }
 }
 
 export default AllTeamsView
