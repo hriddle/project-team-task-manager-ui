8bb1498dcc162de0939f80e641bbef2537e8795d
Heather Riddle <heather0809@gmail.com>
2019-05-25 21:08:25 -0500

    Show due dates and buttons on hover [#165746686]

--- src/dashboard/Task.js
+++ src/dashboard/Task.js
@@ -31,8 +31,26 @@ class Task extends Component {
     this.props.deleteTask(this.props.id);
   }
 
+  getFormattedMonth(date) {
+    let mm = date.getMonth() + 1;
+    if (mm < 10) {
+      mm = `0${mm}`;
+    }
+    return mm;
+  }
+
+  getFormattedDayOfMonth(date) {
+    let dd = date.getDate();
+    if (dd < 10) {
+      dd = `0${dd}`;
+    }
+    return dd;
+  }
+
   render() {
     const editing = this.state.editing;
+    const hasDueDate = this.props.task.dueDate !== null;
+    const dueDate = new Date(this.props.task.dueDate);
     return (
       <li className="task" key={this.props.id}>
         <input type="checkbox" />
@@ -50,9 +68,24 @@ class Task extends Component {
             {this.props.task.name}
           </div>
         )}
-        <div className="indicators"/>
+        <div className="indicators">
+          {!hasDueDate ? <div className="placeholder"/> : (
+            <div className="due-date">
+              <div className="month">{this.getFormattedMonth(dueDate)}</div>
+              <div className="day">{this.getFormattedDayOfMonth(dueDate)}</div>
+            </div>
+            )}
+          <div className="placeholder"/>
+        </div>
         <div className="task-buttons">
           <i className="button material-icons" onClick={this.handleDelete}>delete</i>
+          <i className="button material-icons" onClick={this.beginEditing}>edit</i>
+          {hasDueDate ? (
+            <div className="placeholder"/>
+          ) : (
+            <i className="button material-icons">calendar_today</i>
+          )}
+            <div className="placeholder"/>
         </div>
       </li>
     )
