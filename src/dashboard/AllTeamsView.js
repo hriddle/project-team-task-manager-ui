3a1f59beb04e25603f06fe107fe502e6b83245ae
Paul Warren <pawarren89@gmail.com>
2019-05-27 21:11:53 -0500

    show if a user has joined a team on the team list page [#165588819]

--- src/dashboard/AllTeamsView.js
+++ src/dashboard/AllTeamsView.js
@@ -22,17 +22,18 @@ class AllTeamsView extends Component {
       .catch(err => alert(`Fetching teams was unsuccessful:\n\n${err}`))
   }
 
-  joinTeam(teamID, userID){
+  joinTeam(teamID, userID) {
     return fetch(`${process.env.REACT_APP_API_HOST}/teams/${teamID}/${userID}`, {
       method: 'PUT',
       headers: {'Content-Type': 'application/json'}
     }).then(res => res.json())
-        .then( team => {
-            let tempTeams = this.state.teams;
-            tempTeams[teamID] = team;
-            this.setState({teams: tempTeams})}
-            )
-        .catch(err => alert(`Joining team was unsuccessful:\n\n${err}`));
+      .then(team => {
+          let tempTeams = this.state.teams;
+          tempTeams[teamID] = team;
+          this.setState({teams: tempTeams})
+        }
+      )
+      .catch(err => alert(`Joining team was unsuccessful:\n\n${err}`));
   }
 
   render() {
@@ -40,11 +41,16 @@ class AllTeamsView extends Component {
     if (this.state.teams.length > 0) {
       teams = this.state.teams.map(team =>
         <li className="team" key={team.id}>
-          <div className="team-name" >{team.name}</div>
-          <div id={team.id} className="join" onClick={e => this.joinTeam(team.id, this.props.userId)}>JOIN</div>
+          <div className="team-name">{team.name}</div>
+          {team.members.filter(member => member.id === this.props.userId).length > 0 ? (
+            <div id={team.id} className="member-of-team">MEMBER</div>
+          ) : (
+            <div id={team.id} className="join" onClick={() => this.joinTeam(team.id, this.props.userId)}>JOIN</div>
+          )}
           <div className="team-divider"/>
         </li>
       );
+      debugger
     }
     return (
       <div className="teams">
